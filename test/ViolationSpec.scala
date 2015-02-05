811fe4208bc5dc5d57fcf4b89100a74c3cf64503
Mike Hoffert <mike.hoffert@usask.ca>
2015-02-05 06:47:46 -0600

    Removed dummy comment

--- test/ViolationSpec.scala
+++ test/ViolationSpec.scala
@@ -6,10 +6,6 @@ import play.api.test._
 import play.api.test.Helpers._
 import models.Violation
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
 @RunWith(classOf[JUnitRunner])
 class ViolationSpec extends Specification {
    val violation = new Violation(123, "name", "violation description", "priority")
