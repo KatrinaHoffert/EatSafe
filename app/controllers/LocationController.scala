605ee596cfae1614d844832c0c151fe3cf5a8821
Mike Hoffert <mike.hoffert@usask.ca>
2015-02-05 03:37:32 -0600

    Start to creating controllers and routes
    Created stubs for controller functions and the appropriate routing.

---
+++ app/controllers/LocationController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object LocationController extends Controller {
+  /**
+   * TODO: Document me.
+   */
+  def findLocation(city: String) = TODO
+
+  /**
+   * TODO: Document me.
+   */
+  def showLocation(locationId: Int) = TODO
+}
