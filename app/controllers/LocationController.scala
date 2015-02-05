fb631893cd4121b5988e0a18a3f440a039bd73b4
Kat Hoffert <kat.hoffert@gmail.com>
2015-02-05 04:22:52 -0600

    Created initial templates

--- app/controllers/LocationController.scala
+++ app/controllers/LocationController.scala
@@ -7,10 +7,14 @@ object LocationController extends Controller {
   /**
    * TODO: Document me.
    */
-  def findLocation(city: String) = TODO
+  def findLocation(city: String) = Action {
+    Ok(views.html.locations.findLocation())
+  }
 
   /**
    * TODO: Document me.
    */
-  def showLocation(locationId: Int) = TODO
+  def showLocation(locationId: Int) = Action {
+    Ok(views.html.locations.displayLocation())
+  }
 }
