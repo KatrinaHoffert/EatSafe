36a437abea44ca2858597c11dec495a15b3e9c6f
Kat Hoffert <kat.hoffert@gmail.com>
2015-02-05 03:37:32 -0600

    Start to creating controllers and routes
    Created stubs for controller functions and the appropriate routing.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,12 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-
+  /**
+   * The index of the site (served when the user goes to the base URL). This will merely redirect
+   * the user to a different portion of the site (for now). When we implement the city selection,
+   * that will become the new index page.
+   */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.LocationController.findLocation("Saskatoon"))
   }
-
 }
