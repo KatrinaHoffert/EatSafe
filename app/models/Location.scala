5aee626652408fa3b897d87c040e04223c14a3bd
Kat Hoffert <kat.hoffert@gmail.com>
2015-02-05 04:42:25 -0600

    Skeleton code in model

--- app/models/Location.scala
+++ app/models/Location.scala
@@ -1,7 +1,20 @@
 package models
 
+import scala.util.{Try, Success, Failure}
+
 /**
  * TODO: Document me.
  */
 case class Location (id: Int, name: String, address: String, postalCode: String, city: String,
   regionalHealthAuthority: String, inspections: Seq[Inspection])
+
+object Location {
+  /**
+   * TODO: Document me.
+   */
+  def getLocationById(id: Int): Try[Location] = {
+    // TODO -- returning dummy data right now
+    Try(Location(123, "Foo", "123 Fake St", "S1K 2N3", "Saskatoon", "Saskatoon Health Authority",
+        Seq.empty[Inspection]))
+  }
+}
--- app/models/Violation.scala
+++ app/models/Violation.scala
@@ -1,5 +1,7 @@
 package models
 
+import scala.util.{Try, Success, Failure}
+
 /**
  * TODO: Document me!
  */
