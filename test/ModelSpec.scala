3dd909fb0656daa25a2252c10985c476cf12792f
magnusandy <magnusandy@gmail.com>
2015-02-07 12:20:38 -0600

    Finished Model Testing
    These tests are put in place but without a database, i cannot really
    test if the tests or the code they are testing is working.

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -23,8 +23,13 @@ import org.specs2.mock._
 @RunWith(classOf[JUnitRunner])
 class ModelSpec extends Specification with Mockito {
   
-  //  this.getLocationByIdTests();
-    
+  //run test functions here
+  this.getViolationsTests();
+  this.getInspectionsTests();
+  this.getLocationByIdTests();
+  this.getLocationsByCityTests();
+
+  
   def getLocationByIdTests()
   {
     
@@ -33,43 +38,52 @@ class ModelSpec extends Specification with Mockito {
     //as for now, they will be the same data anyway so i wont worry 
     "getLocationById" should {
       
-      "return a success with proper data when given good data" in new WithApplication{
+      "return a success with proper data when given good data" in {
       //Test good data and expected results here
        
         val goodLoc = Location.getLocationById(1)
         var pass = false
          goodLoc match {
-           case Success(v) => pass = true
-           case Failure(e) => pass = false
+           case Success(loc) => pass = true
+           case Failure(e) =>
+           { 
+             pass = false;
+             "error message" mustEqual e.getMessage()
+           }
          }
        pass mustEqual true
       }
       //data here is based off of the database files
-    "return good data when given a good ID" in new WithApplication
+    "return good data when given a good ID" in 
     {
         val goodLoc = Location.getLocationById(2)
         var pass = false
          goodLoc match
          {
-           case Success(v) =>
+           case Success(loc) =>
            {
-               v.id must beEqualTo(2)// cause thats what i passed in
-               v.name must beEqualTo("7 Eleven") .ignoreSpace //ignore space due to trailing whitespace
-               v.address must beEqualTo("835 A Broadway AVE") .ignoreSpace //ignore space due to trailing whitespace
-               v.postalCode must beEqualTo("S7N 1B5") .ignoreSpace //ignore space due to trailing whitespace   
+               loc.id must beEqualTo(2)// cause thats what i passed in
+               loc.name must beEqualTo("7 Eleven") .ignoreSpace //ignore space due to trailing whitespace
+               loc.address must beEqualTo("835 A Broadway AVE") .ignoreSpace //ignore space due to trailing whitespace
+               loc.postalCode must beEqualTo("S7N 1B5") .ignoreSpace //ignore space due to trailing whitespace   
                pass = true
            }
-           case Failure(e) => pass = false
+           case Failure(e) =>
+           {
+             pass = false
+             //this prints the error message within the test
+             "error message" mustEqual e.getMessage()
+           }
          } 
         pass mustEqual true
     }
-    "return failure with a bad id (empty result set)" in new WithApplication{
+    "return failure with a bad id (empty result set)" in {
       //Test good data and expected results here
        
         val goodLoc = Location.getLocationById(445)
         var pass = false
          goodLoc match {
-           case Success(v) => pass = false
+           case Success(loc) => pass = false
            case Failure(e) => pass = true
          }
         pass mustEqual true
@@ -82,15 +96,183 @@ class ModelSpec extends Specification with Mockito {
   }
   }
   
-    def getLocationsByCityTests()
+  def getLocationsByCityTests()
   {
-    /*
-     * todo, run function in a number of senarios and do tests based on that
-     * run with good inputs, bad inputs, possibly without network connection.
-     * find or create a database record with no violations perhaps.
-     * 
-     */
+    "GetLocationsByCity" should
+    {
+      "return a success when given 'Saskatoon'" in 
+      {
+      //Test good data and expected results here
+       
+        val goodLocList = Location.getLocationByCity("Saskatoon")
+        var pass = false
+         goodLocList match {
+           case Success(locList) => pass = true
+           case Failure(e) =>
+           { 
+             pass = false;
+             "error message" mustEqual e.getMessage()
+           }
+         }
+       pass mustEqual true
+      }
+      //based on current database
+      "return a sequence of Locations with 22 values when given 'Saskatoon'" in 
+      {
+        val goodLocList = Location.getLocationByCity("Saskatoon")
+        var pass = false
+         goodLocList match {
+           case Success(locList) => 
+           {
+             pass = true
+             locList.length must beEqualTo(22)
+           }
+           case Failure(e) => 
+           {
+             "error message" mustEqual e.getMessage()
+             pass = false
+           }
+         }
+       pass mustEqual true
+      }
+      "return failure when given anything but 'Saskatoon'" in 
+      {
+        val goodLocList = Location.getLocationByCity("This City Does NOT Exist")
+        var pass = false
+         goodLocList match {
+           case Success(locList) => 
+           {
+             pass = false
+           }
+           case Failure(e) => 
+           {
+             pass = true
+           }
+         }
+       pass mustEqual true
+        
+      }
+    }   
   }
   
+def getViolationsTests()
+{
+  "getViolations" should
+  {
+    "return success when given proper inputs" in
+      {
+      //Test good data and expected results here
+       
+      //TODO get good data
+        val vioList = Location.getViolations(2, "TODODATE")
+        var pass = false
+         vioList match {
+           case Success(vio) => pass = true
+           case Failure(e) =>
+           { 
+             pass = false;
+             "error message" mustEqual e.getMessage()
+           }
+         }
+       pass mustEqual true
+      }
+      //based on current database
+      "given a certian id, the number of returned violations should be correct" in 
+      {
+        val vioList = Location.getViolations(2, "TODODATE")
+        var pass = false
+         vioList match {
+           case Success(vio) => 
+           {
+             pass = true
+             vio.length must beEqualTo(22)
+           }
+           case Failure(e) => 
+           {
+             "error message" mustEqual e.getMessage()
+             pass = false
+           }
+         }
+       pass mustEqual true
+      }
+      "return failure when given bad data" in 
+      {
+        val vioList = Location.getViolations(9001, "2019-01-17")
+        var pass = false
+         vioList match {
+           case Success(vio) => 
+           {
+             pass = false
+           }
+           case Failure(e) => 
+           {
+             pass = true
+           }
+         }
+       pass mustEqual true
+        
+      }
+  }
+}
+
+def getInspectionsTests()
+{
+  "getInspections" should
+  {
+    "return success when given proper inputs" in 
+      {
+      //Test good data and expected results here
+       
+      
+        val InspList = Location.getInspection(2)
+        var pass = false
+         InspList match {
+           case Success(insp) => pass = true
+           case Failure(e) =>
+           { 
+             pass = false;
+             "error message" mustEqual e.getMessage()
+           }
+         }
+       pass mustEqual true
+      }
+      //based on current database
+      "given a good id, return the right ammount of inspections" in 
+      {
+        val InspList = Location.getInspection(2)
+        var pass = false
+         InspList match {
+           case Success(insp) => 
+           {
+             pass = true
+             insp.length must beEqualTo(22/*TODO get the right number*/)
+           }
+           case Failure(e) => 
+           {
+             "error message" mustEqual e.getMessage()
+             pass = false
+           }
+         }
+       pass mustEqual true
+      }
+      "return failure when given bad data" in 
+      {
+        val vioList = Location.getInspection(9001)
+        var pass = false
+         vioList match {
+           case Success(vio) => 
+           {
+             pass = false
+           }
+           case Failure(e) => 
+           {
+             pass = true
+           }
+         }
+       pass mustEqual true
+        
+      }
+  }
+}
   
 }
