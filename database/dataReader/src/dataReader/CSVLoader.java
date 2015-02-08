24275cbdb293e62bd6570d5170392b78c7d86ca2
LujieDuan <66813185@qq.com>
2015-02-07 21:26:24 -0600

    update dataReader
    all the SQL statements are in statements.sql;
    To construct DB, run CreateTables.sql, then statements.sql
    See issue #22 for detail

--- database/dataReader/src/dataReader/CSVLoader.java
+++ database/dataReader/src/dataReader/CSVLoader.java
@@ -7,6 +7,8 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.apache.commons.lang.WordUtils;
+
 import au.com.bytecode.opencsv.CSVReader;
 
 public class CSVLoader {
@@ -50,6 +52,8 @@ public class CSVLoader {
 					+ e.getMessage());
 		}
 
+		
+		//get rid of the header row of each file
 		String[] headerRow = csvReader.readNext();
 
 		if (null == headerRow) {
@@ -60,7 +64,8 @@ public class CSVLoader {
 		}
 
 		String[] nextLine;
-
+		
+		// a 2D matrix for input data
 		ArrayList<String[]> dataMatrix = new ArrayList<String[]>();
 		String str = "";
 		
@@ -74,7 +79,7 @@ public class CSVLoader {
 					int index = 0;
 					
 					for (String string : nextLine) {
-						date = DateUtil.convertToDate(string);
+						date = DateUtil.convertToDate(string); // try to see if the string is a date
 						if (null != date) {
 							recordLine[index] = date + str;
 						} else {
@@ -88,34 +93,36 @@ public class CSVLoader {
 					
 			// Insert location
 			this.writer.write(String.format("INSERT INTO location(id, name, address, postcode, city, rha)\n"
-					+ " VALUES (%d, %s, %s, %s, %s, %s)\n\n", 
-					locationId, dataMatrix.get(0)[1], dataMatrix.get(0)[3], dataMatrix.get(0)[5], dataMatrix.get(0)[5], dataMatrix.get(0)[7]));
-			
+					+ " VALUES (%d, \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');\n\n", 
+					locationId, WordUtils.capitalizeFully(dataMatrix.get(0)[1]).replaceAll("'","-"), // location ID in unique and increased by one, initial cap the location name
+					dataMatrix.get(0)[3], dataMatrix.get(0)[5].substring(dataMatrix.get(0)[5].length() - 7), //get the postcode, which is the last 7 char
+					dataMatrix.get(0)[5].substring(0, dataMatrix.get(0)[5].lastIndexOf(',')),  // get the city name, which is before the comma
+					dataMatrix.get(0)[7]));
+				//There is not a good way to escape single quote without using prepareStatement, So just use '-' for single quote
 			
 			for (int i = 0; i < dataMatrix.size(); i ++){
 				
 				if(i == 0 || !(dataMatrix.get(i)[2].equals(dataMatrix.get(i-1)[2]))){
 					
 					//Insert inspection
-					this.writer.write(String.format("INSERT INTO inspection(location_id, inspection_date, inspection_type, reinspection_priority)\n"
-							+ " VALUES (%d, %s, %s, %s)\n\n", 
-							inspectionId, dataMatrix.get(i)[2], dataMatrix.get(i)[4], dataMatrix.get(i)[6]));
-					inspectionId ++;
+					this.writer.write(String.format("INSERT INTO inspection(id, location_id, inspection_date, inspection_type, reinspection_priority)\n"
+							+ " VALUES (%d, %d, \'%s\', \'%s\', \'%s\');\n\n", 
+							inspectionId, locationId, dataMatrix.get(i)[2], dataMatrix.get(i)[4], dataMatrix.get(i)[6]));
+					inspectionId ++;// after insert the new inspection, should increase the inspectionId by one, to guarantee the uniqueness
 					if(!dataMatrix.get(i)[9].equals("")){
 						//Insert violation
 						this.writer.write(String.format("INSERT INTO violation(inspection_id, violation_id)\n"
-								+ " VALUES (%d, %s)\n\n", 
-								inspectionId - 1, dataMatrix.get(i)[9]));
+								+ " VALUES (%d, %s);\n\n", 
+								inspectionId - 1, dataMatrix.get(i)[9].substring(0, dataMatrix.get(i)[9].lastIndexOf('-') - 1)));
 					}
 					
-				}else{
+				}else{//a violation for the same inspection
 					if(!dataMatrix.get(i)[9].equals("")){
 						//Insert violation
 						this.writer.write(String.format("INSERT INTO violation(inspection_id, violation_id)\n"
-								+ " VALUES (%d, %s)\n\n", 
-								inspectionId - 1, dataMatrix.get(i)[9]));
+								+ " VALUES (%d, %s);\n\n", 
+								inspectionId - 1, dataMatrix.get(i)[9].substring(0, dataMatrix.get(i)[9].lastIndexOf('-') - 1)));
 					}
-					System.err.println(dataMatrix.get(i)[9]);
 				}
 			}
 		} catch (Exception e) {
@@ -125,7 +132,6 @@ public class CSVLoader {
 							+ e.getMessage());
 		} finally {
 			csvReader.close();
-			
 		}
 		return inspectionId;
 	}
