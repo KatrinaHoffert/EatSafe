4bdbdf495853a73908f74bca9fa57b62249aaf82
magnusandy <magnusandy@gmail.com>
2015-02-16 14:56:07 -0600

    some commenting
    describing the test hooks a lil

---
+++ app/globals/ActiveDatabase.scala
@@ -0,0 +1,9 @@
+package globals
+
+/**
+ * A special class to be used implicitly by any method that accesses the database
+ * this class simply holds the name of a database stored in the framework. (see conf/application.conf)
+ * the purpose if to provide testing mechanism so that a testing database can be used for testing and a
+ * deployment or default database is used for everything else
+ */
+class ActiveDatabase(val name: String)
