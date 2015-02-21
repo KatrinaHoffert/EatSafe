e3c1d758ea517b6c2a935bdbdf572350894935e3
Katherine Jamieson (kmj098) <kmj098@mail.usask.ca>
2015-02-20 20:20:35 -0600

    Updated application tests to check all current views

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -12,17 +12,27 @@ import play.api.test.Helpers._
  */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
-
+  
   "Application" should {
-
+    
     "send 404 on a bad request" in new WithApplication{
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "render the Saskatoon page" in new WithApplication{
+   "render the Saskatoon page" in new WithApplication{
       val home = route(FakeRequest(GET, "/find/Saskatoon")).get
 
       contentType(home) must beSome.which(_ == "text/html")
+    }
+    
+   "show the find location page for Saskatoon" in new WithApplication{
+       val saskatoon = route(FakeRequest(GET, "/find/Saskatoon")).get
+       contentType(saskatoon) must beSome.which(_ == "text/html")
+    }
+    
+    "show the view locations page for a valid id" in new WithApplication{
+      val view = route(FakeRequest(GET, "/view/1")).get
+      contentType(view) must beSome.which(_ == "text/html")
     }
   }
 }
