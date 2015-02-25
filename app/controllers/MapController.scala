9b7ac3828c159948515fd8948ca7919a420b0f8d
davidzlocker <quentinjayp@gmail.com>
2015-02-25 17:36:25 -0600

    Removed unused code

--- app/controllers/MapController.scala
+++ app/controllers/MapController.scala
@@ -26,32 +26,6 @@ object MapController extends Controller {
 	}
 
 
-	/* This is nor working as of yet. I need to find a way to make this work so that the geoencoding can be done in the backend
-
-	//takes in an address and retunrs a lat long
-	// I will have to do better error checking than I have done here.
-	def geocodeAddress(address: String): Option[(Double, Double)] = {
-
-			implicit val timeout = Timeout(50000 milliseconds)
-	
-			val encodedAddress = this.formatAddress(address)
-
-			val latitudeAndLongitude = WS.url("http://maps.googleapis.com/maps/api/geocode/json?address=" + encodedAddress + "&sensor=true").get()
-   
-			val future = latitudeAndLongitude map {
-      			response => (response.json \\ "location")
-    		}
-
-    		// Wait until the future completes (Specified the timeout above)
-
-    		val result = Await.result(future, timeout.duration).asInstanceOf[List[play.api.libs.json.JsObject]]
-
-    		val latitude = (result(0) \\ "lat")(0).toString.toDouble
-    		val longitude = (result(0) \\ "lng")(0).toString.toDouble
-    		return Option(latitude, longitude)
-
-	}
-	*/
 
 
 	def showMap(address: String, city: String) = Action {
