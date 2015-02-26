aa6452bbbc958355b777e1b003c9f23bf06d2620
Mike Hoffert <mike.hoffert@usask.ca>
2015-02-25 18:51:19 -0600

    Special case to catch "c/o" and remove it

--- app/controllers/MapController.scala
+++ app/controllers/MapController.scala
@@ -1,22 +1,19 @@
 package controllers
 
-import scala.util.{Try, Success, Failure} 
 import play.api._
 import play.api.mvc._
 import models._
-import models.Location
-import globals.Globals.defaultDB
-
-import java.net.URLEncoder
-import play.api.libs.ws.WS
-import play.api.Play.current
-import akka.util.Timeout
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ Await, Future }
-import scala.concurrent.duration._
+import scala.util.{Try, Success, Failure}
+import scala.util.matching._
 
 object MapController extends Controller {
   def showMap(address: String, city: String) = Action {
-    Ok(views.html.locations.displayMap(address, city))
+    // Remove bad parts of addresses. Currently we just know about "c/o", so we'll remove that here.
+    var cleanAddress = address;
+    if(address.toLowerCase.startsWith("c/o")) {
+      val coMatcher = """[Cc][\\/][Oo]""".r // Case insensitive "c/o" and "c\o"
+      cleanAddress = coMatcher replaceAllIn(address, m => "")
+    }
+    Ok(views.html.locations.displayMap(cleanAddress, city))
   }
 }
