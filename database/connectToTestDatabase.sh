b24e3d9eb950e985d5c69c75938c8505a3b0666b
magnusandy <magnusandy@gmail.com>
2015-02-28 00:42:04 -0600

    Fixed the .sh files
    that "probably mike" guy, what a sucker, have to go and fix his errors
    all the time

--- database/connectToTestDatabase.sh
+++ database/connectToTestDatabase.sh
@@ -1,4 +1,6 @@
-onnects to the remote database with a Postgres client. psql must be in the PATH.
+#!/usr/bin/env bash
+
+# Connects to the remote database with a Postgres client. psql must be in the PATH.
 export PGPASSWORD=M4PGx3zUySvMmB9j
 
 psql --host=cmpt371g1.usask.ca --dbname=eatsafe_test --username=eatsafe
