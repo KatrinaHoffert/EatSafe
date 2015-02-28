fd987efcd06560231e812dda347e79177ded6f8c
Kat Hoffert <kat.hoffert@gmail.com>
2015-02-28 02:29:23 -0600

    Cleaner errors (and can be dismissed on click)

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -4,50 +4,28 @@
  * This file is part of EatSafe Saskatchewan.
  */
 
-/** Used by createPageError() for storing the timer. */
-var timeoutForPageError;
-
-/** Used by createPageError() to show when the error is being removed (but is still there). */
-var topErrorIsBeingHidden = false;
-
 /**
- * Displays an error message at the top of the user's viewport. This will last 3 seconds before
- * disappearing on its own. Only one of these can be created at a time. Attempting to create another
- * wilk just overwrite the message and reset the timeout.
+ * Displays an error message at the top of the user's viewport. This will last ~3 seconds before
+ * disappearing on its own. Only one of these can be displayed at a time. Attempting to create another
+ * will remove the previous.
  *
- * @param errorMessage Plain text message to display.
+ * @param errorMessage Message to display.
  */
 function createPageError(errorMessage) {
-  // Creates the timer that will hide the error
-  function createTimeout() {
-    return window.setTimeout(function() {
-      topErrorIsBeingHidden = false;
-      $(".topViewError").fadeAndRemove("slow", function() {
-        topErrorIsBeingHidden = false;
-      });
+  // Destroy any existing errors
+  $(".topViewError").fadeAndRemove("fast");
+
+  var errorHtml = $('<div class="alert alert-danger alert-dismissible topViewError" role="alert" data-dismiss="alert">' +
+      errorMessage + '</div>').hide();
+
+  $(document.body).append(errorHtml);
+
+  var errorDiv = $(".topViewError");
+  errorDiv.fadeIn("slow", function() {
+    window.setTimeout(function() {
+      errorDiv.fadeAndRemove("slow");
     }, 3000);
-  }
-
-  // Check if error is already being displayed. If so, just change the text and reset the timer.
-  // But if the error is currently being hidden, it's too late to stop the timer -- create a new
-  // one.
-  if(!topErrorIsBeingHidden || $(".topViewError").length == 0) {
-    window.clearTimeout(timeoutForPageError);
-
-    var errorHtml = $("<div>").hide();
-    errorHtml.addClass("topViewError");
-    errorHtml.text(errorMessage);
-
-    $(document.body).append(errorHtml);
-
-    timeoutForPageError = createTimeout();
-    $(".topViewError").fadeIn("slow");
-  }
-  else {
-    window.clearTimeout(timeoutForPageError);
-    errorHtml.text(errorMessage);
-    timeoutForPageError = createTimeout();
-  }
+  });
 }
 
 /**
