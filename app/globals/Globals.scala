d34ad0d866973d7ddf63f3e6c2e59ce2c52509d8
Mike Hoffert <mike.hoffert@usask.ca>
2015-02-27 23:23:05 -0600

    Globals change -- only one global ActiveDatabase
    The tests adapted accordingly. This allows us to import globals._ on all
    files that need globals. Tests can either import the database or use an
    implicit ActiveDatabase.

--- app/globals/Globals.scala
+++ app/globals/Globals.scala
@@ -1,13 +1,8 @@
-package globals
-
-package object Globals {
-  
+package object globals {
   /**
    * Any method that includes a implicit ActiveDatabase needs access to one of these implicit values
-   * any classes that are not testing classes should import globals.Globals.defaultDB and while testing classes (in the test folder)
-   * import globals.Globals.testDB
+   * Any classes that are not testing classes should import globals.Globals.defaultDB and while
+   * testing classes (in the test folder) import globals.Globals.testDB.
    */
   implicit lazy val defaultDB = new ActiveDatabase("default")
-  
-  implicit lazy val testDB = new ActiveDatabase("test")
 }
