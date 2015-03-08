4fd3593e20674646f95eb31d85cc521d4f4cddea
Katherine Jamieson (kmj098) <kmj098@mail.usask.ca>
2015-03-07 20:20:17 -0600

    start of ui testing

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,8 +5,14 @@ import play.api.test._
 import play.api.test.Helpers._
 import org.fluentlenium.core.FluentPage
 import org.fluentlenium.core.filter.FilterConstructor._
+
 import org.openqa.selenium._
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox._
+import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.JavascriptExecutor
+
 import play.api.i18n.Messages
 
 /**
@@ -45,20 +51,70 @@ class ApplicationSpec extends Specification {
   "select city page" should {
     "show a typeahead for cities" in new WithBrowser {
       browser.goTo("/")
-      browser.pageSource() must contain(Messages("locations.selectCity.prompt"))
+      browser.pageSource must contain(Messages("locations.selectCity.prompt"))
       browser.getDriver.findElement(By.id("municipality"))
     }
     
-    "display choose a location page when location is submitted" in new WithBrowser {
+    "give error message when trying submit without input" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+      val typeahead = browser.getDriver.findElement(By.id("municipality"))
+      typeahead.click
+      typeahead.sendKeys(Keys.ENTER)
+      browser.$(".topViewError").getText must contain(Messages("locations.selectCity.noInput"))
+    }
+    
+    "give error message when trying to search for invalid place" in new WithBrowser {
       browser.goTo("/")
       val typeahead = browser.getDriver.findElement(By.id("municipality"))
-      typeahead.click()
+      typeahead.click
+      typeahead.sendKeys("asdfghjkl")
+      typeahead.sendKeys(Keys.ENTER)
+      browser.$(".topViewError").getText must contain(Messages("locations.selectCity.badInput"))
+    }
+    
+    "display choose location page when location is typed in all caps" in new WithBrowser {
+      browser.goTo("/")
+      val typeahead = browser.getDriver.findElement(By.id("municipality"))
+      typeahead.click
+      typeahead.sendKeys("SASKATOON")
+      typeahead.sendKeys(Keys.ENTER)
+      browser.url must contain("/find/Saskatoon")
+    }
+    
+     "display choose location page when location is typed in all lowercase" in new WithBrowser {
+      browser.goTo("/")
+      val typeahead = browser.getDriver.findElement(By.id("municipality"))
+      typeahead.click
+      typeahead.sendKeys("saskatoon")
+      typeahead.sendKeys(Keys.ENTER)
+      browser.url must contain("/find/Saskatoon")
+    }
+    
+    "display choose location page when location is fully typed and submitted with enter" in new WithBrowser {
+      browser.goTo("/")
+      val typeahead = browser.getDriver.findElement(By.id("municipality"))
+      typeahead.click
       typeahead.sendKeys("Saskatoon")
       typeahead.sendKeys(Keys.ENTER)
-      browser.url() must contain("/find/Saskatoon")
+      browser.url must contain("/find/Saskatoon")
     }
-  }
-  
+    
+    "display choose location page when location is partially typed, hint is clicked and submitted with enter" in new WithBrowser {
+      browser.goTo("/")
+      val typeahead = browser.getDriver.findElement(By.id("municipality"))
+      val action = new Actions(browser.getDriver)
+      typeahead.click
+      typeahead.sendKeys("Saskato")
+      action.moveToElement(typeahead).perform
+      val element = browser.webDriver.findElement(By.linkText("Saskatoon"))
+      action.moveToElement(element)
+      action.click
+      action.perform
+      typeahead.sendKeys(Keys.ENTER)
+      browser.url must contain("/find/Saskatoon")
+    }
+  } 
+
   "select location page" should {
     "show a typeahead for locations" in new WithBrowser {
       browser.goTo("/find/Saskatoon")
@@ -72,7 +128,7 @@ class ApplicationSpec extends Specification {
       typeahead.click()
       typeahead.sendKeys("2nd Avenue Grill")
       typeahead.sendKeys(Keys.ENTER)
-      browser.url() must contain("/view/3059")
+      browser.url() must contain("/view/")
     }
   }
 }
