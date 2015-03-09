5e6059f85b3e3700841129da65e2cf28fb5efd3e
Kat Hoffert <kat.hoffert@gmail.com>
2015-03-09 17:06:54 -0600

    Implemented link to about page
    Also removed unused controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,12 +5,10 @@ import play.api.mvc._
 
 object Application extends Controller {
   /**
-   * The index of the site (served when the user goes to the base URL). This will merely redirect
-   * the user to a different portion of the site (for now). When we implement the city selection,
-   * that will become the new index page.
+   * Displays information about how freaking cool we are.
    */
-  def index = Action {
-    Redirect(routes.LocationController.selectCity())
+  def about = Action {
+    Ok("We be cool, like yo")
   }
 
   /**
