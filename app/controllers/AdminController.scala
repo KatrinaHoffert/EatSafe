10d4f508c5e750bb2cda040f1d8321cb52c315c0
Kat Hoffert <kat.hoffert@gmail.com>
2015-03-25 17:40:31 -0600

    Start to authentication system

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -6,8 +6,45 @@ import play.api.mvc._
 import models._
 import globals._
 import play.api.Logger
+import play.api.data._
+import play.api.data.Forms._
 
 object AdminController extends DetectLangController {
+  val loginForm = Form(
+    tuple(
+      "username" -> text,
+      "password" -> text
+    )
+    verifying ("Invalid username or password", result => result match {
+      case (username, password) => checkPassword(username, password)
+    })
+  )
+
+  def checkPassword(username: String, password: String) = {
+    (username == "admin" && password == "1234")  
+  }
+
+  def login = Action { implicit request =>
+    Ok(views.html.admin.login())
+  }
+
+  def logout = Action {
+    Redirect(routes.AdminController.login).withNewSession.flashing(
+      "loggedOut" -> ""
+    )
+  }
+
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors =>
+        Redirect(routes.AdminController.login).flashing(
+          "badPassword" -> ""
+        ),
+      user =>
+        Redirect(routes.AdminController.listAllLocations).withSession(Security.username -> user._1)
+    )
+  }
+
   /**
    * Displays search results.
    */
