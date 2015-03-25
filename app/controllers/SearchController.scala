0b09903604a9bcc3750d68ada3fd9ae3d660a24c
Mike Hoffert <mike.hoffert@usask.ca>
2015-03-24 21:44:50 -0600

    Created search controller and start of page
    No view yet, but page correctly shows a list of locations.

---
+++ app/controllers/SearchController.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import scala.util.{Try, Success, Failure} 
+import play.api._
+import play.api.mvc._
+import models._
+import globals._
+import play.api.Logger
+
+object SearchController extends DetectLangController {
+  /**
+   * Displays search results.
+   */
+  def searchLocations(query: String, city: String) = Action { implicit request =>
+    SearchEngine.queryLocations(query, city) match {
+      case Success(cities) =>
+        Ok(cities.toString)
+      case Failure(ex) =>
+        Logger.error("Search system failed with query \"\"\"" + query + "\"\"\"", ex)
+        InternalServerError(views.html.errors.error500(ex))
+    }
+  }
+}
