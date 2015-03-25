811700e99e05c4e344a1b23f99e8f2b403114b0c
Kat Hoffert <kat.hoffert@gmail.com>
2015-03-25 01:33:28 -0600

    Search is now properly ranking
    More obvious with the laxer search (top results will be more relevant).
    Also cleaned up some related code, commented the SQL file, and changed
    the SQL to weigh the name heavier than the address.

--- app/models/SearchEngine.scala
+++ app/models/SearchEngine.scala
@@ -13,6 +13,8 @@ object SearchEngine {
   /**
    * Queries the locations with a full text name and address search utilizing Postgres's TSVECTOR
    * type, which allows for natural language full text searches. It allows for a fast, fuzzy search.
+   * Output is ordered by how good of a match it is according to ts_rank (note that the name is
+   * weighted slightly higher than the address).
    *
    * @param query The string that the user used as a search query.
    * @param city The name of the city to search in. Use "Unknown city" for locations without a city.
@@ -27,26 +29,21 @@ object SearchEngine {
     Try {
       DB.withConnection(db.name) { implicit connection =>
         val tsQuery = toTsQuery(query, lax)
-        val sqlQuery = if(city.toLowerCase != "unknown city") {
-          SQL(
+        val cityConditional = if(city.toLowerCase != "unknown city") {
+            "LOWER(city) = LOWER({city})"
+          } else {
+            "city IS NULL"
+          }
+
+        val sqlQuery = SQL(
            """
-            SELECT id, name, address
+            SELECT id, name, address, ts_rank(text_vector, to_tsquery('eatsafe_english', {query})) AS rank
               FROM location_search
               INNER JOIN location ON(location_id = id)
-              WHERE LOWER(city) = LOWER({city}) AND text_vector @@ to_tsquery('eatsafe_english', {query});
+              WHERE """ + cityConditional + """ AND text_vector @@ to_tsquery('eatsafe_english', {query})
+              ORDER BY rank DESC;
            """
           ).on("city" -> city, "query" -> tsQuery)
-        }
-        else {
-          SQL(
-           """
-            SELECT id, name, address
-              FROM location_search
-              INNER JOIN location ON(location_id = id)
-              WHERE city IS NULL AND text_vector @@ to_tsquery('eatsafe_english', {query});
-           """
-          ).on("city" -> city, "query" -> tsQuery)
-        }
         
         sqlQuery().map {
           row => SlimLocation(row[Int]("id"), row[String]("name"), row[Option[String]]("address"))
--- database/CreateSearchTable.sql
+++ database/CreateSearchTable.sql
@@ -1,7 +1,11 @@
+# Remove existing objects that we're about to create to prevent conflicts.
 DROP TABLE IF EXISTS location_search CASCADE;
 DROP TEXT SEARCH CONFIGURATION IF EXISTS eatsafe_english CASCADE;
 DROP TEXT SEARCH DICTIONARY IF EXISTS eatsafe_synonyms CASCADE;
 
+# We need to apply our own synonyms dictionary so that users can, for example, get results like
+# "8th st" when they search for "8th street". Doing so requires that we copy the english text
+# search configuration because we can't alter english's configuration with an unprivileged user.
 CREATE TEXT SEARCH CONFIGURATION eatsafe_english (
   COPY = english
 );
@@ -15,6 +19,13 @@ ALTER TEXT SEARCH CONFIGURATION eatsafe_english
   ALTER MAPPING FOR asciiword
   WITH eatsafe_synonyms, english_stem;
 
+# The location_search table maps TSVECTORS (which contain lexemes) to location IDs (so we can figure
+# out what location we matched). The location name and address are both used in the TSVECTOR, with
+# the name being weighted heavier. Note that since the address might be null, we must provide a
+# default (an empty TSVECTOR), as the concatenation of TSVECTORS will fail if we try and concat
+# a NULL.
 CREATE TABLE location_search AS
-  SELECT id AS location_id, to_tsvector('eatsafe_english', concat_ws(' ', name, address)) AS text_vector
+  SELECT id AS location_id, setweight(to_tsvector('eatsafe_english', name), 'A')
+      || COALESCE(setweight(to_tsvector('eatsafe_english', address), 'B'),
+      to_tsvector('eatsafe_english', '')) AS text_vector
   FROM location;
