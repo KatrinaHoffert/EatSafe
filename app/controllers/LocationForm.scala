4358f15429dc10b52f192e937fe0679ecfcaea19
Kat Hoffert <kat.hoffert@gmail.com>
2015-03-25 23:03:29 -0600

    Validation for the form
    Users shouldn't be able to submit bad stuff now.

--- app/controllers/LocationForm.scala
+++ app/controllers/LocationForm.scala
@@ -7,7 +7,13 @@ import play.api.data._
 import play.api.data.Forms._
 import org.apache.commons.lang3.time.DateFormatUtils
 import models.Location
+import globals._
+import play.api.i18n.Messages
 
+/**
+ * Represents the data that the user must supply to add or edit a location. It's not a perfect match
+ * to the Location object because the user does not use IDs.
+ */
 case class LocationForm(
   name: String,
   address: Option[String],
@@ -51,6 +57,37 @@ object LocationForm {
       "rha" -> nonEmptyText,
       "inspections" -> seq(inspectionMapping)
     )(LocationForm.apply)(LocationForm.unapply)
+    verifying (Messages("admin.validation.invalidPostalCode"), fields => {
+      fields.postalCode.map(_.matches("""[A-Za-z][0-9][A-Za-z] ?[0-9][A-Za-z][0-9]""")).getOrElse(true)
+    })
+    verifying (Messages("admin.validation.invalidRha"), fields => {
+      validRhas.contains(fields.rha)
+    })
+    // While we could do this validation at the inspectionMapping level, we won't because Play
+    // won't treat the error like a global error and we'll have to do more work in displaying the
+    // error to the user.
+    verifying (Messages("admin.validation.invalidInspectionType"), fields => {
+      fields.inspections.map { inspection =>
+        validInspectionTypes.contains(inspection.inspectionType)
+      }.foldRight(true)(_ && _)
+    })
+    verifying (Messages("admin.validation.invalidReinspectionPriority"), fields => {
+      fields.inspections.map { inspection =>
+        validReinspectionPriorities.contains(inspection.reinspectionPriority)
+      }.foldRight(true)(_ && _)
+    })
+    verifying (Messages("admin.validation.invalidViolationIds"), fields => {
+      fields.inspections.map { inspection =>
+        // Matches a comma-separated list, eg, "1,2,3" or "12, 123 ,8"
+        inspection.violations.map(_.matches("""^(\d+\s*,\s*)*\d+$""")).getOrElse(true)
+      }.foldRight(true)(_ && _) &&
+      fields.inspections.flatMap { inspection =>
+        inspection.violationIds.map { violationId =>
+          // There's only 16 IDs numbered 1 through 16. Anything else is invalid.
+          violationId >= 1 && violationId <= 16
+        }
+      }.foldRight(true)(_ && _)
+    })
   )
 
   /**
