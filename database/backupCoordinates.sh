673b3640610a5d5efd8590c05125160e83d0f208
Mike Hoffert <mike.hoffert@usask.ca>
2015-03-26 19:38:56 -0600

    Created scripts to do all DB work

---
+++ database/backupCoordinates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script backs up the coordinates from the coordinates table in the database into a local file
+# which will be restored by the `createDatabase.sh` script (which also re-creates the database, and
+# thus the need for this script). If you've updated the coordinates by running the coordinate
+# getter auxillary program, then you should run this script prior to running createDatabase.sh
+# (if the coordinates haven't been changed, you don't need to run this script).
+
+#################
+# CONFIGURATION #
+#################
+DBHOST=cmpt371g1.usask.ca
+DBNAME=eatsafe
+USERNAME=eatsafe
+PASSWORD=M4PGx3zUySvMmB9j
+# End of configuration
+
+export PGPASSWORD="$PASSWORD"
+
+psql --host="$DBHOST" --dbname="$DBNAME" --username="$USERNAME" -f "backupCoordinates.sql"
