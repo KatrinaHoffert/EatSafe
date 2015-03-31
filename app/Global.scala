93827d65641bf40e440a94b6b94664994c0fd15e
LujieDuan <66813185@qq.com>
2015-03-31 14:03:47 -0600

    fix 400 and 404 language
    issue #171

--- app/Global.scala
+++ app/Global.scala
@@ -10,13 +10,14 @@ import play.api.Logger
  * Bceause this property messes with the tests (we don't want the global ActiveDatabase to be
  * used in the tests), this object is used solely for the special hooks described in
  */
-object Global extends GlobalSettings {
+object Global extends GlobalSettings with controllers.DetectLangController{
   /**
    * HTTP 500 errors. Ideally, this one should never happen. All our errors are supposed to be
    * manually caught at the controller level, to allow for more specific logging and error handling.
    * If this ever happens, it's likely a bug on our end.
    */
   override def onError(request: RequestHeader, ex: Throwable) = {
+    implicit val requestImplicit = request
     Logger.error("User encountered unexpected error. This is probably a bug on our end.", ex)
     Future.successful(InternalServerError(views.html.errors.error500(ex)))
   }
@@ -25,6 +26,7 @@ object Global extends GlobalSettings {
    * HTTP 404 errors.
    */
   override def onHandlerNotFound(request: RequestHeader) = {
+    implicit val requestImplicit = request
     Logger.warn("User hit 404 at " + request.path)
     Future.successful(NotFound(views.html.errors.error404()))
   }
@@ -33,6 +35,7 @@ object Global extends GlobalSettings {
    * HTTP 400 errors.
    */
   override def onBadRequest(request: RequestHeader, error: String) = {
+    implicit val requestImplicit = request
     Logger.warn("User hit 400 at " + request.path + "\nError message is: " + error)
     Future.successful(BadRequest(views.html.errors.error400()))
   }
