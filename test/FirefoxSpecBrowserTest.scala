3a00264049803eba600cd19d1a76451ab76c45c3
Katherine Jamieson <kjamieson098@gmail.com>
2015-04-03 17:10:21 -0600

    added checks to recently added pages

--- test/FirefoxSpecBrowserTest.scala
+++ test/FirefoxSpecBrowserTest.scala
@@ -62,7 +62,25 @@ class FirefoxSpecBrowserTest extends Specification {
     action.click.perform
     browser.pageSource must contain (Messages("about.title"))
     
-    //TODO multimap page
+    
+     //400 error page
+    browser.goTo("/view/99999999999999999")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("footer.aboutLink")))).perform
+    action.click.perform
+    browser.pageSource must contain (Messages("about.title"))
+    
+    //404 error page
+    browser.goTo("/dfgjnsddjdk")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("footer.aboutLink")))).perform
+    action.click.perform
+    browser.pageSource must contain (Messages("about.title"))
+    
+    //search page
+    browser.goTo("/search/saskatoon?q=subway")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("footer.aboutLink")))).perform
+    action.click.perform
+    browser.pageSource must contain (Messages("about.title"))
+   
    
   }  
   
@@ -91,7 +109,17 @@ class FirefoxSpecBrowserTest extends Specification {
     browser.goTo("/find/daDerpDaDerp")
     action.moveToElement(browser.webDriver.findElement(By.linkText("CC-BY-ND"))).perform
      
-   //TODO multimap page
+     //400 error page
+    browser.goTo("/view/99999999999999999")
+    action.moveToElement(browser.webDriver.findElement(By.linkText("CC-BY-ND"))).perform
+    
+    //404 error page
+    browser.goTo("/dfgjnsddjdk")
+    action.moveToElement(browser.webDriver.findElement(By.linkText("CC-BY-ND"))).perform
+    
+        //search results page
+    browser.goTo("/search/saskatoon?q=subway")
+    action.moveToElement(browser.webDriver.findElement(By.linkText("CC-BY-ND"))).perform
   }  
   
   
@@ -145,7 +173,30 @@ class FirefoxSpecBrowserTest extends Specification {
     action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("general.applicationName")))).perform
     action.click.perform
     browser.url must contain ("/")
-    //TODO multimap page
+    
+    //400 error page
+    browser.goTo("/view/99999999999999999")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("general.applicationName")))).perform
+    action.click.perform
+    browser.url must contain ("/")
+    
+    //404 error page
+    browser.goTo("/dfgjnsddjdk")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("general.applicationName")))).perform
+    action.click.perform
+    browser.url must contain ("/")
+    
+    //search page
+    browser.goTo("/search/saskatoon?q=subway")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("general.applicationName")))).perform
+    action.click.perform
+    browser.url must contain ("/")
+    
+    // multi-map page
+    browser.goTo("/citymap?city=Saskatoon")
+    action.moveToElement(browser.webDriver.findElement(By.linkText(Messages("general.applicationName")))).perform
+    action.click.perform
+    browser.url must contain ("/")
      
   }
     
